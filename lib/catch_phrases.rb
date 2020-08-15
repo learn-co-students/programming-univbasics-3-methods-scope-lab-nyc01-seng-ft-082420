68dc8b0d1c152a2f598e3c4a97103b94e4bb5adb
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-15 21:42:24 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,113 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+
+def
+  
+  all_phrases
+
+  puts "ALL THE PHRASES ::
+  "
+  
+  mario
+  toadstool
+  link
+
 end
 
-def toadstool
-  puts status
+
+
+
+def 
+  
+  mario
+  # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  # puts phrase
+
+
+  puts "It's-a me, Mario!"
+
 end
+
+
+
+
+def
+  
+  toadstool
+  
+
+  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
+  puts phrase
+
+end
+
+
+
+
+def
+  
+  link
+
+
+  phrase = "It's Dangerous To Go Alone! Take This."
+  
+  puts phrase
+
+end
+
+
+
+
+all_phrases
+
+
+
+
+
+
+
+
+##########
+
+
+
+
+
+
+
+
+# ** (Global Variables)
+# ** (Local Variables)
+#
+# **  **  Trying to Call One Another From Inside One Another -- Out of Scope!
+
+
+
+
+# def 
+  
+#   mario
+  
+  
+#   phrase = "It's-a me, Mario!"
+  
+  
+#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
+#   puts phrase
+
+# end
+
+
+
+
+# def 
+  
+#   toadstool
+  
+
+#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+    
+#   puts status
+
+# end
+
